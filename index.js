4e965444feed0c550bad6304924906a3aafb5bdc
onurravli <onurravli@hotmail.com>
2023-11-26 16:01:39 +0300

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const { ApolloServer, gql } = require("apollo-server-express");
+const express = require("express");
+const port = 3000;
+const app = express();
+
+const users = [
+  {
+    id: 1,
+    name: "Onur",
+    surname: "Ravli",
+    username: "onurravli",
+    active: true,
+  },
+  {
+    id: 2,
+    name: "John",
+    surname: "Doe",
+    username: "johndoe",
+    active: false,
+  },
+];
+
+const typeDefs = gql`
+  type User {
+    id: Int!
+    name: String!
+    surname: String!
+    username: String!
+    active: Boolean!
+  }
+
+  type Query {
+    users: [User]
+    userById(id: Int!): User
+    userByUsername(username: String!): User
+  }
+`;
+
+const resolvers = {
+  Query: {
+    users: () => users,
+    userById: (_, { id }) => users.find((user) => user.id === id),
+    userByUsername: (_, { username }) => users.find((user) => user.username === username),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start().then(() => {
+  server.applyMiddleware({ app });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}${server.graphqlPath}`);
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+## GraphQL Test API
+
+This repo contains a simple GraphQL API for understanding and demonstrating how GraphQL APIs work
+
+### Installation and Starting
+
+First, install dependent packages by `pnpm install`. Then start the server by `node index.js`. Now you're able to send requests to **http://localhost:3000/graphql** enpoint.
+
+### License
+
+MIT License. For more, look [license.md](license.md).
